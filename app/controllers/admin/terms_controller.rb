a8c3219978e439640b6ded0310dc1f2f3f7356e1
dutavi <tangvinhduong99@gmail.com>
2019-07-03 03:09:21 +0700

    Done termDashboard

---
+++ app/controllers/admin/terms_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+module Admin
+  class TermsController < Admin::ApplicationController
+  end
+end
---
+++ app/dashboards/term_dashboard.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require "administrate/base_dashboard"
+
+class TermDashboard < Administrate::BaseDashboard
+
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    word: Field::String,
+    meaning: Field::String,
+    reading: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  COLLECTION_ATTRIBUTES = [
+  :id,
+  :word,
+  :meaning,
+  :reading
+  ].freeze
+
+  SHOW_PAGE_ATTRIBUTE = [
+  :id,
+  :word,
+  :meaning,
+  :reading,
+  :created_at,
+  :updated_at,
+  ].freeze
+
+  FORM_ATTRIBUTES = [
+    :word,
+    :meaning,
+    :reading,
+  ].freeze
+end
