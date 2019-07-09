bf6eae4ea40ddb80ca8acccead45c755efb2bbea
dutavi <tangvinhduong99@gmail.com>
2019-07-09 11:07:53 +0700

    Fix test

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
+    resources :terms
 
-    root to: "users#index"
+    root to: "users#index" do
+    root to: "terms#index"
+    end
   end
   root to: "visitors#index"
   devise_for :users
-  resources :users
+  resources :users, :terms
 end
