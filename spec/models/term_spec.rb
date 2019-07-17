d151724bd0f577c6334b8a06676d3be1391e28b1
dutavi <tangvinhduong99@gmail.com>
2019-07-17 15:18:40 +0700

    official term/spec.rb

--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -5,17 +5,20 @@ require "rails_helper"
 RSpec.describe Term, type: :model do
   #Test_Word :
   it { should have_db_index :word }
-  it { should have_db_column(:word).with_options null: false }
   it { should validate_presence_of(:word) }
   it { should validate_length_of(:word).is_at_most(255) }
+  it { should have_db_column(:word).with_options null: false }
+
   #Test_Meaning :
-  it { should have_db_column(:meaning).with_options null: true }
   it { should validate_presence_of(:meaning) }
-  it { should have_db_column(:reading).with_options null: true }
+  it { should validate_length_of(:meaning).is_at_most(255) }
+  it { should have_db_column(:meaning).with_options null: true }
+
   #Test_Reading :
   it { should validate_presence_of(:reading) }
   it { should validate_length_of(:reading).is_at_most(255) }
-  it { should validate_presence_of(:reading) }
+  it { should validate_presence_of(:reading).with_options null: true }
   #end.
+
   it { should have_and_belong_to_many(:phrases) }
 end
